71bf45a65822e310c3b854336777de57a5e1bd1b
Nupomaru <atomic1017@gmail.com>
2020-10-20 18:59:34 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,6 +5,10 @@ $name = $_POST["name"];
 $dsn = 'database.cgbqtfdk1vhj.ap-northeast-1.rds.amazonaws.com';
 $user = 'admin';
 $password = 'kvls1093SS';
+$mysqli = new mysqli(dsn, userm password, "test_DB");
 
+if( $mysqli->connect_errno ) {
+	echo $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
+}
 
 ?>
