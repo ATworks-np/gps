5b678e47133a06714b8ada003179bbfc7cb39e5e
Nupomaru <atomic1017@gmail.com>
2020-10-21 02:39:15 +0900

    r

--- updateAll.php
+++ updateAll.php
@@ -1,6 +1,8 @@
 <?php
 $POST = json_decode(file_get_contents('php://input'), true);
 $user_id = $POST["user_id"];
+$widget_id = $POST["widget_id"];
+
 
 $dbhost = 'database.cgbqtfdk1vhj.ap-northeast-1.rds.amazonaws.com';
 $dbname = 'test_DB';
@@ -11,12 +13,12 @@ try
 {
   $dbh=new PDO($dsn, $user, $password);
   $sql = "SELECT * FROM logdata";
-  //$sql = "SELECT * FROM logdata as d WHERE date=(select max(date) from logdata where user_id = d.user_id)" ;
+  //$sql = "SELECT * FROM logdata as d WHERE date=(select max(date) from logdata where widjet_id = d.widjet_id)" ;
 
   $res = $dbh->query($sql);
 
   foreach( $res as $value ) {
-    echo "$value[lat]<br>";
+    echo "$value[id].'：'.value[date].'：'.value[user_id].'：'.value[widjet_id].'：'.value[lat].'：.value[lng]<br>";
   }
 
 }catch (Exception $e)
