63cc01dbae9a28ccbc52ca72032cdd1aad876320
Nupomaru <atomic1017@gmail.com>
2020-10-20 17:56:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,18 @@
 $id = $_GET[‘id’];
 echo $id;
 ?>
+
+<!DOCTYPE html>
+<html lang = “ja”>
+<head>
+<meta charset = “UFT-8”>
+<title>フォームからデータを受け取る</title>
+</head>
+<body>
+<h1>フォームデータの送信</h1>
+<form action = “index.php” method = “get”>
+<input type = “text” name =“id/“><br/>
+<input type = “submit” value =“送信/“>
+</form>
+</body>
+</html>
